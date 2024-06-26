a9b60263e69df55046449a7bf6a43532b8c8cd80
Ubuntu <ubuntu@ip-172-31-40-3.ap-south-1.compute.internal>
2024-06-26 06:37:09 +0000

    Updated s3 module

--- s3-module/main.tf
+++ s3-module/main.tf
@@ -2,3 +2,10 @@
 resource "aws_s3_bucket" "example" {
   bucket = var.bucketname
 }
+# Enabled verison in s3
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
